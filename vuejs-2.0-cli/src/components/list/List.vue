dde9fe532fff893f517ce58cd0b55019c45903ad
陈小龙 <13480731740@qq.com>
2016-12-05 21:12:58 +0800

    列表添加完成

--- vuejs-2.0-cli/src/components/list/List.vue
+++ vuejs-2.0-cli/src/components/list/List.vue
@@ -2,28 +2,20 @@
     <div class="list">
         <div class="am-panel am-panel-default">
             <div class="am-panel-hd">
-                <h3 class="am-panel-title">创悦保险列表</h3>
+                <h3 class="am-panel-title">创悦保险列表 {{todoCount}}</h3>
             </div>
             <div class="am-panel-bd">
-                <form class="am-form">
+                <form class="am-form" v-on:submit.prevent="addTodo(newTodo)">
                         <div class="am-form-group">
-                            <input type="text" class="am-form-field" placeholder="输入保险名称">
+                            <input type="text" class="am-form-field" placeholder="输入保险名称" v-model="newTodo.title">
                             <button type="submit" class="am-btn am-btn-primary am-btn-block">提交</button>
                         </div>
                 </form>
             </div>
             <ul class="am-list am-list-static am-list-border">
-                <li>
-                    <span class="am-badge am-badge-success">YES</span> <span class="am-badge am-badge-danger">NO</span>
-                    <a href="#">人保保险</a>
-                </li>
-                <li>
-                    <span class="am-badge am-badge-success">YES</span> <span class="am-badge am-badge-danger">NO</span>
-                    <a href="#">人保保险</a>
-                </li>
-                <li>
-                    <span class="am-badge am-badge-success">YES</span> <span class="am-badge am-badge-danger">NO</span>
-                    <a href="#">人保保险</a>
+                <li v-for="(todo,index) in todos">
+                        <button class="am-badge am-badge-danger" v-on:click="deleteTodo(index)">Del</button>
+                    <a href="#">{{todo.title}}</a>
                 </li>
             </ul>
         </div>
@@ -32,7 +24,35 @@
 
 <script>
 export default {
-  name: 'list'
+
+    name: 'list',
+
+    data(){
+      return{
+          todos:[
+              {id:1,title:'平安保险'}
+          ],
+          newTodo:{id:null,title:''}
+      }
+    },
+
+    computed:{
+        todoCount(){
+            return this.todos.length;
+        }
+    },
+
+    methods:{
+        addTodo(newTodo)
+        {
+            this.todos.push(newTodo),
+                this.newTodo ={id:null,title:''}
+        },
+        deleteTodo(index){
+            this.todos.splice(index,1)
+        }
+    }
+
 };
 </script>
 
